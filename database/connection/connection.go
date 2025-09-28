dad94a07b1e92d5f0798cd222aa4d6484f1b75f8
rendysyahputra <rendysyahputra33@gmail.com>
2025-09-29 00:55:03 +0700

    rev railway

--- database/connection/connection.go
+++ database/connection/connection.go
@@ -6,29 +6,19 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var Db *sql.DB
 
 func ConnectDB() {
-
-	_ = godotenv.Load("config/.env")
-
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
-	)
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		log.Fatal("DATABASE_URL tidak ditemukan")
+	}
 
 	var err error
-	Db, err = sql.Open("postgres", psqlInfo)
+	Db, err = sql.Open("postgres", databaseUrl)
 	if err != nil {
 		log.Fatal("Gagal buka koneksi:", err)
 	}
--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"api-book/database/connection"
 	"api-book/router"
+	"os"
 )
 
 func main() {
-	var PORT = ":8080"
 
 	connection.ConnectDB()
 
-	router.StartServer().Run(PORT)
+	router.StartServer().Run(":" + os.Getenv("PORT"))
 }
